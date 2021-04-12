fc511365afa1141a1b2d4de83a099acbd671f203
ArizonaPeachTea <zuxingp888@gmail.com>
2021-04-12 13:05:06 -0500

    navigating to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,28 +89,29 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Luigi Mountain',
-                address: {
-                    street: 'Paradise 3rd',
-                    zipCode: '41351',
-                    country: 'Germany'
-                },
-                email: 'luigi@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        // this.setState({ loading: true });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Luigi Mountain',
+        //         address: {
+        //             street: 'Paradise 3rd',
+        //             zipCode: '41351',
+        //             country: 'Germany'
+        //         },
+        //         email: 'luigi@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // };
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     })
+        //     .catch(error => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     });
+        this.props.history.push('/checkout');
     };
 
     render() {
