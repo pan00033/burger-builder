e99edf5fb19da26bac9c8074e8387893c1eaba72
ArizonaPeachTea <zuxingp888@gmail.com>
2021-04-19 13:10:51 -0500

    adding the auth form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,6 +7,7 @@ const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger House</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
+        <NavigationItem link="/auth">Log In</NavigationItem>
     </ul>
 );
 
