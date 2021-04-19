731b55d2466039229362281ca3f4cc9c58fed689
ArizonaPeachTea <zuxingp888@gmail.com>
2021-04-18 22:13:26 -0500

    handling purchases and updating the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,8 +4,10 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
+
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -18,8 +20,10 @@ class Checkout extends Component {
     render() {
         let summary = <Redirect to="/" />;
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -36,8 +40,12 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     };
 };
+
+
+
 
 export default connect(mapStateToProps)(Checkout);
